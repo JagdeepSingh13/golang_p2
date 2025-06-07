926a3a47a102f01edb3e91f417d03c8a13b75778
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-06-07 16:40:30 +0530

    react-app

---
+++ 05_todo/client/src/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+import TodoList from "To-Do-List";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
---
+++ 05_todo/server/go.mod
@@ -0,0 +1,3 @@
+module github.com/JagdeepSingh13/05_todo
+
+go 1.24.2
